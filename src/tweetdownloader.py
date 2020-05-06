468cf76e70afb5f610bbf0d2bdf3c178f7f6f428
Heath Gordon <angrydogproductions@gmail.com>
2020-05-06 14:00:32 +0200

    basic poisson

--- src/tweetdownloader.py
+++ src/tweetdownloader.py
@@ -2,10 +2,14 @@ import configparser
 from pathlib import Path
 import logging
 from collections import defaultdict
+import datetime
+import time
 
 import tweepy
 import pandas as pd
 
+# move to main one?
+logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
 
 class TweetDownloader():
@@ -13,30 +17,92 @@ class TweetDownloader():
     def __init__(self, to_load="all"):
 
         self.api = self.make_api()
-        self.to_load = "realDonaldTrump"
-
+        self.to_load = to_load
 
         self.dataset = self.load_dataset(self.to_load)
 
         if type(self.dataset) == type(Path()):
-            print("no dataset")
+            self.download_dataset()
+            self.write_new_dataset()
+
         elif type(pd.DataFrame()) == type(self.dataset):
             self.update_dataset()
+            self.write_updated_dataset()
+
+    def write_updated_dataset(self):
+        logger.info(f"Writing {self.file_path}")
+        self.dataset.to_csv(self.file_path)
+
+    def write_new_dataset(self):
+        df = pd.DataFrame(self.new_tweets)
+        df.to_csv(self.dataset, index=False)
 
 
+    def update_tweets_dict_short(self, tweet):
+        labels = ["created_at", "id", "text"]
+
+        for label in labels:
+            self.new_tweets[label].append(tweet[label])
+
+
+    def download_dataset(self):
+        """Get the last 120 days of tweets"""
+        logger.info(f"Downloading: {self.to_load}")
+        
+        # get target date
+        target_date = datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(days=90)
+
+        self.new_tweets = defaultdict(list)
+        count = 0
+        exit_ = False
+        while True:
+            logger.info(f"\tPage: {count}")
+            if count <1:
+                time.sleep(1)
+                tweets = self.api.user_timeline(screen_name=self.to_load,
+                                                count=200)
+            else:
+                time.sleep(1)
+                tweets = self.api.user_timeline(screen_name=self.to_load,
+                                                count=200,
+                                                max_id=max_id)
+
+            for tweet in tweets:
+                tweet_json = tweet._json
+                self.update_tweets_dict_short(tweet_json)
+
+                if pd.to_datetime(tweet_json["created_at"]) < target_date:
+                    print('woop')
+                    exit_ = True
+                    break
+
+
+            if exit_:
+                break
+            logger.info(f"\tDate: {tweet_json['created_at']}")
+            max_id = tweet_json["id"]
+            count += 1
+            
+        
     def combine_datasets(self, new_tweets):
-        print(new_tweets["source"])
+
         df = pd.DataFrame.from_dict(new_tweets)
         df.index = pd.to_datetime(df["created_at"])
-        print(df.index[0])
-        print(df.index[0].tz)
+        df.index = df.index.tz_localize(None)
 
         self.dataset = pd.concat([self.dataset, df])
-        #self.dataset.sort_index()
+        self.dataset.sort_index(inplace=True, ascending=False)
+
+        # check for duplicates
+        test = self.dataset.duplicated(['id_str'], keep='first')
+        logger.info(f"Found: {test.sum()} duplicate rows")
+
+        self.dataset = self.dataset[~test]
+
 
     def update_tweets_dict(self, new_tweets, tweet):
 
-        labels = ["source", "text", "created_at", "retweet_count", "id"]
+        labels = ["source", "text", "created_at", "retweet_count", "id_str"]
 
         for label in labels:
             new_tweets[label].append(tweet[label])
@@ -50,14 +116,18 @@ class TweetDownloader():
         # get most recent id
         last_id = int(self.dataset.iloc[0]["id_str"])
         new_tweets = defaultdict(list)
+        logger.info(f"Liberating {self.to_load}")
 
         count = 0
         exit_ = False
         while True:
+            logger.info(f"\tPage: {count}")
             if count < 1:
+                time.sleep(1)
                 tweets = self.api.user_timeline(screen_name=self.to_load,
                                                 count=200)
             else:
+                time.sleep(1)
                 tweets = self.api.user_timeline(screen_name=self.to_load,
                                                 count=200,
                                                 max_id=max_id)
@@ -104,6 +174,7 @@ class TweetDownloader():
             # load dataset
             df = pd.read_csv(self.file_path)
             df.index = pd.to_datetime(df["created_at"])
+            df.sort_index(inplace=True, ascending=False)
 
             return df
         else:
@@ -146,7 +217,9 @@ class TweetDownloader():
         return api
 
 def main():
-    td = TweetDownloader()
+    TweetDownloader(to_load='realDonaldTrump')
+    TweetDownloader(to_load='whitehouse')
+    TweetDownloader(to_load='potus')
 
 
 if __name__ == "__main__":
--- src/utils.py
+++ src/utils.py
@@ -2,8 +2,8 @@ from pathlib import Path
 
 import pandas as pd
 
-def rdt_tweets(amount='basic'):
-    tweet_path = Path("data/realDonaldTrump.csv")
+def tweets(to_load, amount='basic'):
+    tweet_path = Path("data")
 
     while True:
         if tweet_path.exists():
@@ -11,15 +11,44 @@ def rdt_tweets(amount='basic'):
         else:
             tweet_path = ".." / tweet_path
 
+    file_name = to_load + ".csv"
+    tweet_path = tweet_path / file_name
+
     df = pd.read_csv(tweet_path)
 
     df.index = pd.to_datetime(df["created_at"])
 
     if amount == 'basic':
 
-        df = pd.DataFrame(df['id_str'])
+        try:
+            df = pd.DataFrame(df['id_str'])
+        except:
+            df = pd.DataFrame(df["id"])
         df.columns = ['ID']
 
     return df
+
+
+def prior_bucket_maker(data, lower, buckets=9, gap=10):
+    
+    prior_counts = dict()
+    
+    for i in range(buckets):
+        if i == 0:
+            prior_counts[str(lower)] = sum(data <= lower)
+        elif i == buckets -1:
+
+            lower_bound = (lower + i * gap) - (gap - 1)
+            prior_counts[str(lower_bound)] = sum(data >= lower_bound)
+        else:
+            lower_bound = (lower + i * gap) - (gap - 1)
+            upper_bound = (lower + i * gap)
+            
+            label = f"{lower_bound}-{upper_bound}"
+            
+            prior_counts[label] = sum((data >= lower_bound) & (data <= upper_bound))
+            
+    return prior_counts
+        
 
     
