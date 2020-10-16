d59a54d3edeaefb474c8f58e2123a61c212f81b3
Heath Gordon <angrydogproductions@gmail.com>
2020-10-16 17:39:15 +0200

    Readme, started cleaning

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# trump_tweets
+# Gambling on Trump Tweets
+
+Until June of 2020, on the website [predictit.org](https://predictit.org), one could
+gamble on the number of tweets that [Donald Trump](https://twitter.com/realdonaldtrump),
+the [whitehouse](https://twitter.com/WhiteHouse) or [POTUS](https://twitter.com/POTUS/)
+twitter accounts would make over the course of a week.
+My gut instinct tells me that this was an inefficient market, and that one could fit 
+a distribution to his past tweets and then go from there.
+
+**NOTE:** this will not work if you get it up and running, but I will leave it up as
+a reference point. 
--- trump_tweets.py
+++ trump_tweets.py
@@ -12,30 +12,40 @@ import src.poisson as poisson
 from src.tweetdownloader import TweetDownloader
 import src.gengamma as gengamma
 
+"""NOTE: this is no longer how I solve this problem.
+Now I would have a utils file with something like this:
+root_path = Path(os.path.abspath(__file__)).parent.parent
+which could be imported here"""
 cwd = Path(__file__)
 os.chdir(cwd.parent)
 
 def main():
 
+    # load header material for html
     fin_html = utils.html_head
     
+    # loop through each of the tweet markets
     for market in utils.market_numbers:
 
+        # NOTE: since 
         print(market)
 
         tweets = TweetDownloader(to_load=market)
 
-        # get response_data
+        # get price data from website 
         auction_data = utils.price_data(utils.market_numbers[market])
         data = utils.price_info(auction_data)
+
+        # get the number of tweets that have been tweeted already
         tweets_so_far = len(tweets.dataset[:data.start_date])
+
         cutoff_data = utils.setup_data(tweets.dataset["id"])
 
         prices_df = pd.DataFrame.from_dict(data.prices,
                                            orient="index")
         prices_neg_df = pd.DataFrame.from_dict(data.prices_neg,
                                                orient="index")
-        
+
         odds = poisson.basic_poisson(cutoff_data,
                                      data.days_left,
                                      tweets_so_far,
@@ -48,7 +58,7 @@ def main():
                                        data.lower,
                                        data.gap)
 
-
+        # write lists etc to html, not sure it's the correct way
         str_io = StringIO()
 
         h1 = f"<h1>{market}</h1>\n"
@@ -71,7 +81,6 @@ def main():
 
         fin_html = fin_html + h2 + html_str
 
-        
     fin_html = fin_html + utils.html_end
 
     with open('index.html', 'w') as out_file:
